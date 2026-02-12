92c47ad372995bcab25246319fd3f427100bd877
James Freiwirth <james@freiwirth.co.uk>
2026-02-12 13:59:09 +0000

    Try again

--- package.nix
+++ package.nix
@@ -84,6 +84,9 @@ config_file="''${1:-$out/share/hid-digital-lcd-controller/config.json}"
 exec ${python3}/bin/python3 -c "
 import sys
 sys.path.insert(0, '$out/lib/python3.13/site-packages')
+sys.path.insert(0, '${python3.pkgs.numpy}/lib/python3.13/site-packages')
+sys.path.insert(0, '${python3.pkgs.hid}/lib/python3.13/site-packages')
+sys.path.insert(0, '${python3.pkgs.psutil}/lib/python3.13/site-packages')
 from digital_thermal_right_lcd.controller import main
 main('$config_file')
 "
