d258045e094716c9dab10497774464199c5584ce
James Freiwirth <james@freiwirth.co.uk>
2026-02-12 14:11:54 +0000

    debug

--- package.nix
+++ package.nix
@@ -80,7 +80,11 @@ export PKG_CONFIG_PATH="${libdrm.dev}/lib/pkgconfig:\$PKG_CONFIG_PATH"
 export LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib:${glibc}/lib:${zlib}/lib:${hidapi}/lib:\$LD_LIBRARY_PATH"
 
 # Debug and run the controller
-echo "Arguments received: $@"
+echo "Wrapper script called with arguments: $@"
+echo "Number of arguments: $#"
+for i in "$@"; do
+  echo "Arg: $i"
+done
 config_file="$1"
 if [ -z "$config_file" ]; then
   echo "No config file provided, using default"
@@ -89,6 +93,8 @@ else
   echo "Using config file: $config_file"
 fi
 echo "Final config file: $config_file"
+echo "Python version:"
+${python3}/bin/python3 --version
 exec ${python3}/bin/python3 -c "
 import sys
 sys.path.insert(0, '$out/lib/python3.13/site-packages')
